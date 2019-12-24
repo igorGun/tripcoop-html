3d36e295b06e2d3dfbd3499a3285bb3ea5ecf576
Oksana Smolynets <oksana.mavdryk25@gmail.com>
2019-12-24 22:45:55 +0200

    8 fixed

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ var markers = {};
 
 var latlng;
 
-var myLatLng = [{
-  lat: 49.992157,
-  lng: 36.231082,
-}, ];
+var myLatLng = [
+  {
+    lat: 49.992157,
+    lng: 36.231082,
+  },
+];
 for (var i = 0; i < myLatLng.length; i++) {
   console.log(i, myLatLng[i]);
   var marker = new google.maps.Marker({
@@ -100,38 +102,38 @@ function bindMarkerinfo(marker) {
   infowindow.open(map, marker);
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   // open-close main menu
-  $('.btn.btn-primary.toggle-menu-btn').click(function (e) {
+  $('.btn.btn-primary.toggle-menu-btn').click(function(e) {
     $('#mainMenuItems').toggle();
     $('#fa-bars-icon').toggle();
     $('#fa-times-icon').toggle();
   });
 
   // open-close order-block
-  $('.btn.btn-success.open-order-box-btn').click(function (e) {
+  $('.btn.btn-success.open-order-box-btn').click(function(e) {
     $('.order-block').toggle();
-    $('.order-block').css('display') === 'block' ?
-      $(this)
-      .text('Скрить')
-      .css('max-width', '400px')
-      .css('margin', '0 auto') :
-      $(this)
-      .text('Информация поездки')
-      .css('max-width', '100%');
+    $('.order-block').css('display') === 'block'
+      ? $(this)
+          .text('Скрить')
+          .css('max-width', '400px')
+          .css('margin', '0 auto')
+      : $(this)
+          .text('Информация поездки')
+          .css('max-width', '100%');
   });
 
   // close chat-box button
-  $('.close.chat-close-btn').click(function () {
+  $('.close.chat-close-btn').click(function() {
     $('.chat-block').toggle();
     $('#map').css('height', '100%');
   });
 
-  $('.responsive-sign-block').click(function () {
+  $('.responsive-sign-block').click(function() {
     $('.sign-in-window').toggle();
   });
 
-  $('.sign-in-btn-close').click(function () {
+  $('.sign-in-btn-close').click(function() {
     $('.sign-in-window').toggle();
   });
 });
@@ -166,6 +168,5 @@ function deleteTextButton() {
   } else {
     $('#set-trip-button').text('Искать поездку');
   }
-
 }
 $(window).resize(deleteTextButton);
